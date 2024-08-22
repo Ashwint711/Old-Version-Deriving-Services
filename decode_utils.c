016199d56867870c5643b8d5d05560e00d010650
Ashwint711 <ashwintirpude74@gmail.com>
2024-08-22 17:13:03 +0530

    Fixed problem where program was failing to decode when number of Total Bits are multiple of 8

--- decode_utils.c
+++ decode_utils.c
@@ -11,6 +11,8 @@ void Decode(unsigned char* arr)
 	int bit_seq_len = 0;
 	int i = 0;
 
+/*... FETCHING count of Bits in which Count/Length is stored. ...*/
+
 	while(true)
 	{
 		if( (rem_bits - (cur_bits + 1)) >= 0 )
@@ -41,110 +43,58 @@ void Decode(unsigned char* arr)
 		if(temp & 1) break;
 		cur_bits = val;
 	}
-	//	printf("\nLength is stored in next %d bits.\n",val);
-	//	printf("Total bits for sequence %d \n\n",bit_seq_len);
-	
 
-	// val - Number of bits in which Length/Count is stored.
-	// bit_seq_len - Number of bits taken to store sequence of Length/Count.
+/*... END of FETCHING count of Bits in which Count/Length is stored. ...*/
 	
+	//printf("\nLength is stored in next %d bits.\n",val);
+	//printf("Total bits for sequence %d \n\n",bit_seq_len);
+
+
+	/* val - Number of bits in which Length/Count is stored. */
+	/* bit_seq_len - Number of bits taken to store sequence of Length/Count. */
+
+/*... DECODING actual COUNT/LENGTH ...*/
+
 	//TOTAL bits to store whole encoded sequence
-	unsigned char total_bits = val + bit_seq_len;
-        unsigned char remn = total_bits % 8;//37 % 8 = 5
-        int k = (total_bits/8) + !!(total_bits % 8);//4+1
-        k -= 1 ;//4
-        int size = ((val / 8) + !!(val % 8));//3
-        unsigned char value = 0;
-        unsigned char *res =  (unsigned char*) calloc(size, sizeof(unsigned char));
+	unsigned char total_bits = val + bit_seq_len;//27+13
+	unsigned char remn = total_bits % 8;//40 % 8 = 0
+	int k = (total_bits/8) + !!(total_bits % 8);//5+0
+	k -= 1 ;//4
+	int size = ((val / 8) + !!(val % 8));//3+1
+	unsigned char value = 0;
+	unsigned char *res =  (unsigned char*) calloc(size, sizeof(unsigned char));
 
-        for(int i = (size - 1); i >= 0; i--)//i=2,1,0
-        {
-                *(res + i) = (*(arr + k) >> (8 - remn));
-                k -= 1;
-                if(k < 0) break;
-                *(res + i) |= (*(arr + k) << remn);
-        }
+	for(int i = (size - 1); i >= 0; i--)//i=3,2,1,0
+	{
+		if(remn > 0)
+		{
+			*(res + i) = (*(arr + k) >> (8 - remn));
+			k -= 1;
+			*(res + i) |= (*(arr + k) << remn);
+		}
+		else
+		{
+			*(res + i) = *(arr + k);
+			k -= 1;
+		}
+	}
 
-        *(res) &= (0xff >> ((!!(val % 8)) * (8 - (val % 8))));//Don't shift when remainder is 0.
+	*(res) &= (0xff >> ((!!(val % 8)) * (8 - (val % 8))));//Don't shift when remainder is 0.
+
+/*... END of FETCHING actual COUNT/LENGTH ...*/
 
 
-	//printf("Decoded sequence in bits \n");
+/*... PRINTING DECODED SEQUENCE ...*/
+
 	unsigned long long int count = 0;
 	unsigned char* ptr = (unsigned char*) &count;
 	for(i = 0; i < size; i++)
 	{
-		//print_bits(*(res + i));
-                *(ptr + (size - 1 - i)) = *(res + i);
+		*(ptr + (size - 1 - i)) = *(res + i);
 	}
 	printf("\n");
 	printf("Count is : %llu\n",count);
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/*	unsigned char value = 0;
-	unsigned char terminator = 0;
-	temp = 0;
-	i = bit_seq_len / 8;
-
-	unsigned char rem = (bit_seq_len % 8);
-	terminator = (val / 8) < 1 ? 1 : (val) / 8;
-
-	printf("Decoded Sequence \n");
-	for(int j = 0; j < terminator; j++)
-	{
-		value = *(arr + i) << rem;
-		i += 1;
-		temp = *(arr + i) >> (8 - rem);
-		value |= temp;
-		//printf("%d ",value);
-		print_bits((value>>=(8 - val)));
-		value = 0;
-		temp = 0;
-	
-	}
-*/
-	//val - Total number of bits in which Length is stored.
-/*	int size = (val/8) + (!!(val % 8));
-	unsigned char *out;
-	out = (unsigned char*) calloc(size, sizeof(unsigned char));
-
-	unsigned char one = 1;
-	printf("total_bits is : %d\n",total_bits);
-	one <<= (8 - (total_bits % 8));
-	temp = 0;
-	int j,k = 0;
-	j = size - 1; //Start storing from last byte of out
-	k = (total_bits / 8); //Start scanning from byte where actual length sequence starts
-	for(i = 0; i < val; i++)
-	{
-		temp = (*(arr + k) & one) > 0 ? 1 : 0;
-		*(out + j) |= (temp << (i % 8));
-		one <<= 1;
-		if(one == 0)
-		{
-			one = 1;
-			k -= 1;
-		}
-		if(k < 0) break;
-
-		if((i + 1) / 8 == 1) j -= 1;
-	}
-
-	for(i = 0; i < size; i++){
-		print_bits(*(out + i));
-	}*/
 	printf("\n");
+
+/*... END of PRINTING DECODED SEQUENCE ...*/
 }
--- main.c
+++ main.c
@@ -10,16 +10,22 @@ union un
         unsigned char num_arr[sizeof(unsigned long long int)];
 };
 
+/*
+ *  The Maximum number as a Count this version can work with is : 18446744073709551615
+ *  18446744073709551615 BYTE's [Approx 1 EXABYTE]
+ */
+
 int main()
 {
         unsigned char *in;
-       	unsigned long long int len_in_bytes = 11072000;
+       	unsigned long long int len_in_bytes = 18446744073709551615UL;
         
 	printf("Length/Count : %llu\n",len_in_bytes);
 	printf("\n");
 
 	in = (unsigned char*) malloc(sizeof(unsigned long long int));
 
+/*... STORING from long long int to char array ...*/
 	union un obj;
         obj.num = len_in_bytes;
         int bits = Count_Bits(obj.num);
@@ -30,52 +36,10 @@ int main()
         {
                 *(in + (size - 1 - i)) = obj.num_arr[i];
         }
-	
-/*
-	unsigned char* ptr = (unsigned char*) &len_in_bytes;
-	for(int i = 0; i < 4; i++)
-		print_bits(*(ptr + (4 - 1 - i)));	
-	printf("\n");
-	unsigned char bits_for_len = Count_Bits(len_in_bytes);
-	
-	unsigned char terminate = ((bits_for_len / 8) + (!!(bits_for_len % 8)));
-	unsigned char value,k,remn = 0;
-	remn = (bits_for_len % 8);
-
-// for(int i = 0; i < terminate; i++)
-//	{
-//              *(in + i) = (*(ptr + (terminate - 1 - k)) << (remn > 0 ? 8 - remn : 0)); //removing leading zeros from first byte & also removing leading bit values from next bytes.
-//		k += 1;
-//		if(k == terminate) break;
-//		*(in + i) |= (*(ptr + (terminate - 1 - k)) >> (bits_for_len % 8)); //making space for previously inserted bits in above statement
-//	}
-
-	unsigned char byte_index = 0;
-	unsigned char bit_index = 0;
-	unsigned char bit_count = 0;
-	 for (int i = bits_for_len-1; i >= 0; i--) 
-	 {
-        	int bit = (len_in_bytes >> i) & 1;
-        
-        	// Place bit into the array at the correct position
-        	if (bit) {
-            		*(in + byte_index) |= (1 << (7 - bit_index));
-        	}
-        
-        	bit_index++;
-        	bit_count++;
-        
-        	if (bit_index >= 8) {
-            		bit_index = 0;
-            		byte_index++;
-        	}
-    	}
+/*... END of STORING from long long int to char array ...*/
 	
 
-	
-	unsigned char terminator = (bits_for_len / 8) < 1 ? 1 : (bits_for_len / 8);
-       */ 
-        //Declaration of *out
+       //Declaration of *out
 	unsigned char bits_for_len = Count_Bits(len_in_bytes);
         unsigned char *out;
         out = malloc(sizeof(unsigned char) * ((bits_for_len/8) + 10));
